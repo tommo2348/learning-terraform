85bf77bf48083fcb7e058ca2ebaf991cb59c5b25
tommo2348 <32296854+tommo2348@users.noreply.github.com>
2022-12-28 14:38:23 +0000

    fix syntax

--- main.tf
+++ main.tf
@@ -36,10 +36,10 @@ module "blog_sg" {
 
   vpc_id             = data.aws_vpc.default.id
   ingress_rules      = ["http-80-tcp", "https-443-tcp"]
-  ingrss_cidr_blocks = [0.0.0.0/0]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules      = ["all-all"]
-  egress_cidr_blocks = [0.0.0.0/0]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
