ca1858945e7b78b86bb313bf6f478218292b457a
tommo2348 <32296854+tommo2348@users.noreply.github.com>
2022-12-28 12:33:52 +0000

    using declared instance type

--- variables.tf
+++ variables.tf
@@ -1,11 +1,4 @@
-variable "AWS_ACCESS_KEY_ID" {
-  description = "The AWS access key id credential to access amazon aws"
-  type = string
-  default     = "t3.nano"
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "The AWS secret access key credential to access amazon aws"
-  type = string
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
