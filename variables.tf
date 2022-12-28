2534aca100a1359411765ad42198e0d75054fdc4
tommo2348 <32296854+tommo2348@users.noreply.github.com>
2022-12-28 12:38:40 +0000

    added access key

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "The AWS secret access key credential to access amazon aws"
+  type = string
+}
